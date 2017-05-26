1f6ddd96c5922404d4c1739608f6a5013e0c70ef
Aditya Shinde <shinde.aditya386@gmail.com>
2017-05-25 20:55:57 -0400

    Added real time (active) capture.

--- libs/sniffer.py
+++ libs/sniffer.py
@@ -1,12 +1,14 @@
 from scapy.all import *
 from multiprocessing import Process,Queue,Event
 import time
+from traffic import Traffic
 
 class Sniffer(Process):
 
-    def __init__(self,queue):
+    def __init__(self,queue,bground=0):
         super(Sniffer,self).__init__()
         self.timeout=20
+        self.bground=bground
         self.out=queue
         self.FINISHED=Event()
 
@@ -17,12 +19,17 @@ class Sniffer(Process):
         print("=========================================")
         print(packet.payload.show())
 
-    def run(self):
-        self.FINISHED.clear()
-        capture=sniff(iface="mon0",timeout=self.timeout)
-        self.out.put(list(map(bytes,capture)))
-        self.FINISHED.set()
+    def __send_packets(self,packet):
+        self.out.put(bytes(packet))
 
+    def run(self):
+        if self.bground:
+            sniff(iface="mon0",prn=self.__send_packets)
+        else:
+            self.FINISHED.clear()
+            capture=sniff(iface="mon0",timeout=self.timeout)
+            self.out.put(list(map(bytes,capture)))
+            self.FINISHED.set()
 
 
 class Capture:
@@ -48,6 +55,12 @@ class Capture:
         else:
             return False
 
+    def __dot11_management(self,packet):
+        if packet.haslayer(Dot11) and packet.getlayer(Dot11).type==0:
+            return True
+        else:
+            return False
+
     def start_capture(self,duration=20):
         self.sniffer.set_timeout(duration)
         self.sniffer.start()
@@ -60,12 +73,21 @@ class Capture:
     def search(self,packet_type):
         return list(filter(self.packet_types[packet_type],self.packets))
 
+
+class LiveCapture(Capture):
+
+    def __init__(self):
+        Capture.__init__(self)
+        self.sniffer=Sniffer(self.queue,1)
+
+    def start_capture(self):
+        self.sniffer.start()
+        while 1:
+            packet=RadioTap(self.queue.get())
+
 def main():
-    capture=Capture()
-    capture.start_capture(50)
-    capture.finish_capture()
-    for packet in capture.search("all"):
-        print(packet.summary())
+    capture=LiveCapture()
+    capture.start_capture()
 
 if __name__=="__main__":
     main()
